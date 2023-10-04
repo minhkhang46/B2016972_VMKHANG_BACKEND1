06c5e4c2d59a9234452c02616a0c9b7428ac5792
minhkhang46 <khangb2016972@student.ctu.edu.vn>
2023-10-04 14:21:06 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ const app = express();
 const contactsRouter = require("./app/routes/contact.route");
 const ApiError = require("./app/api-error");
 
+
+
 app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
+
+
+
+
 
 // handle 404 response
 app.use((req, res, next) => {
@@ -22,12 +28,12 @@ app.use((err, req, res, next) => {
     // Trong các đoạn code xử lý ở các route, gọi next(error)
     // sẽ chuyển về middleware xử lý lỗi này
     return res.status(err.statusCode || 500).json({
-    message: err.message || "Internal Server Error",
+        message: err.message || "Internal Server Error",
     });
 });
 
 app.get("/",(req, res) => {
-	res.json({ message: "welcome to contact book application."});
+    res.json({ message: "welcome to contact book application."});
 });
 
 module.exports = app
